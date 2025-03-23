529ee7933f99be845e13ed0cd78221c089d28ca7
castrol-web <castrolmkude@gmail.com>
2025-03-23 22:38:11 +0300

    updated

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3001","https://schoolmanagement-frontend-25p7.onrender.com"],
+    origin: ["http://localhost:3001"],
     methods: ['GET', 'PUT', 'DELETE', 'POST'],
   }
 })
